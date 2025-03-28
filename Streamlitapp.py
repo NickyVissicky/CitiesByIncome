01386ac74a28db6f5aa10d691c96ecf87ebfc0f7
NickyVissicky <93458815+NickyVissicky@users.noreply.github.com>
2025-03-27 22:27:20 -0400

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/CT-towns-income-census-2020.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/CT-Places-income-census-2020.csv")
 df.head(5)
 
 df['Per capita income'] = df['Per capita income'].str.replace('$', '').str.replace(',', '').astype(int)
@@ -14,7 +14,7 @@ st.markdown("Just Nicholas Vissicchio")
 
 county = st.selectbox("Select a County", df['County'].unique())
 selectedCounty = df[df['County'] == county]
-st.dataframe(selectedCounty[['Town', 'Median household income']], width=800, height=200)
+st.dataframe(selectedCounty[['Place', 'Median household income']], width=800, height=200)
 
 minIncome = st.number_input(
     "Select the minmum median household income...",
@@ -35,15 +35,15 @@ maxIncome = st.number_input(
 dfRange = selectedCounty[(selectedCounty['Median household income'] >= minIncome) &
                          (selectedCounty['Median household income'] <= maxIncome)]
 
-st.dataframe(dfRange[['Town', 'Median household income']], width=800, height=200)
+st.dataframe(dfRange[['Place', 'Median household income']], width=800, height=200)
 
-bestCities = df.nlargest(5, 'Median household income')[['Town', 'Median household income']]
-worstCities = df.nsmallest(5, 'Median household income')[['Town', 'Median household income']]
+bestCities = df.nlargest(5, 'Median household income')[['Place', 'Median household income']]
+worstCities = df.nsmallest(5, 'Median household income')[['Place', 'Median household income']]
 
 citiesData = pd.concat([bestCities, worstCities])
 
 plt.figure(figsize=(10, 8))
-plt.bar(citiesData['Town'], citiesData['Median household income'])
+plt.bar(citiesData['Place'], citiesData['Median household income'])
 plt.xlabel('City')
 plt.ylabel('Median Income')
 plt.title('Best and Worst CT Cities by Income')
